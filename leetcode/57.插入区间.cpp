843086bfc2e02ad1b1768094c8b3822e9db83b45
whutfaker <1312976124@qq.com>
2020-04-10 14:02:00 +0800

    插入区间

---
+++ leetcode/57.插入区间.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=57 lang=cpp
+ *
+ * [57] 插入区间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+    }
+};
+// @lc code=end
+
